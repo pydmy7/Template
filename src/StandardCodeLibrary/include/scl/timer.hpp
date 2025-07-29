85dd21c9c44fbbd59cb747b20516ff7dfb95ad37
pydmy7 <91300046+pydmy7@users.noreply.github.com>
2025-07-29 14:50:56 +0800

    timer

--- src/StandardCodeLibrary/include/scl/timer.hpp
+++ src/StandardCodeLibrary/include/scl/timer.hpp
@@ -2,17 +2,42 @@
 
 #include <chrono>
 #include <string>
+#include <iostream>
+#include <format>
 
 namespace scl {
 
 class Timer final {
 public:
-    explicit Timer(const std::string& title);
-    ~Timer();
+    explicit Timer(const std::string& title) {
+        title_ = title;
+
+        auto current_time = std::chrono::steady_clock::now();
+        start_time_ = current_time;
+        last_time_ = current_time;
+    }
+
+    ~Timer() {
+        auto current_time = std::chrono::steady_clock::now();
+        auto start_duration = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time_).count();
+        std::cout << std::format("[Timer: {}, Mark: total] Duration from start timestamp: {} ms.", title_, start_duration) << std::endl;
+    }
+
+    void timeStamp(const std::string& mark) {
+        auto current_time = std::chrono::steady_clock::now();
+        auto last_duration = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_time_).count();
+        auto start_duration = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time_).count();
+        if (last_duration != start_duration) {
+            std::cout << std::format("[Timer: {}, Mark: {}] Duration from last timestamp: {} ms.", title_, mark, last_duration) << std::endl;
+        }
+        std::cout << std::format("[Timer: {}, Mark: {}] Duration from start timestamp: {} ms.", title_, mark, start_duration) << std::endl;
+
+        last_time_ = current_time;
+    }
 
 private:
     std::string title_;
-    std::chrono::steady_clock::time_point start_time_;
+    std::chrono::steady_clock::time_point start_time_, last_time_;
 };
 
 }  // namespace scl
--- tests/demo/src/main.cpp
+++ tests/demo/src/main.cpp
@@ -1,3 +1,18 @@
+#include <thread>
+
+#include "scl/timer.hpp"
+
 int main() {
+
+    scl::Timer timer("test");
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    timer.timeStamp("part 1");
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    timer.timeStamp("part 2");
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    timer.timeStamp("part 3");
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    timer.timeStamp("part 4");
+
     return 0;
 }
